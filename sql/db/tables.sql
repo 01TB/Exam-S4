4be056576b6c8000400e90938769801b3576dadf
01TB <tsitohaina.berthin@gmail.com>
2025-07-07 17:13:33 +0300

    Create tables.sql

---
+++ sql/db/tables.sql
@@ -0,0 +1,75 @@
+-- Base de données pour établissement financier
+-- MySQL 11
+
+-- Table des types de prêts
+CREATE TABLE type_pret (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    montant_max DECIMAL(15,2) NOT NULL,
+    montant_min DECIMAL(15,2) NOT NULL,
+    duree_remboursement_max INT NOT NULL,
+    duree_remboursement_min INT NOT NULL,
+    taux DECIMAL(5,2) NOT NULL
+);
+
+-- Table des départements
+CREATE TABLE departement (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL UNIQUE
+);
+
+-- Table des clients
+CREATE TABLE client (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL
+);
+
+-- Table des utilisateurs (employés)
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_departement INT NOT NULL,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_departement) REFERENCES departement(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- Table des prêts accordés
+CREATE TABLE pret (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_client INT NOT NULL,
+    id_type_pret INT NOT NULL,
+    montant_pret DECIMAL(15,2) NOT NULL,
+    montant_remboursement_par_mois DECIMAL(15,2) NOT NULL,
+    montant_total_remboursement DECIMAL(15,2) NOT NULL,
+    duree_remboursement INT NOT NULL,
+    taux DECIMAL(5,2) NOT NULL,
+    date_demande DATE NOT NULL,
+    FOREIGN KEY (id_client) REFERENCES client(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (id_type_pret) REFERENCES type_pret(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- Table de l'historique des pret
+CREATE TABLE historique_pret (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL,
+    id_pret INT NOT NULL,
+    etat ENUM('cree', 'valide', 'refuse') NOT NULL,
+    date_modif TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_user) REFERENCES user(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (id_pret) REFERENCES pret(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+
+-- Table des dépôts
+CREATE TABLE depot (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL,
+    nom_investisseur VARCHAR(200) NOT NULL,
+    montant DECIMAL(15,2) NOT NULL,
+    date_depot TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    description TEXT,
+    FOREIGN KEY (id_user) REFERENCES user(id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
